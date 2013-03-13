9ccca7ccb39f9a645c4388272addb53bc6ec6926
Taggart Ashby <tgashby@calpoly.edu>
2013-03-12 18:24:37 -0700

    Hoping this makes Box blur work...

--- TiltShift.js
+++ TiltShift.js
@@ -16,6 +16,10 @@ var nodejs = (typeof window === 'undefined');
 if (nodejs) {
     WebCL = require('node-webcl');
     Image = require('node-image').Image;
+    clu = require('./clUtils');
+    util = require('util');
+    fs = require('fs');
+    log = console.log;
 };
 
 if (WebCL === undefined) {
@@ -50,20 +54,21 @@ function InitWebCL () {
 
     // CL Context setup
     // Context is the entire OpenCL environment (kernels, devices, memory management,
-    // queues, etc). It is the meaty mc-meaterson for doing most things.
+    // clQueues, etc). It is the meaty mc-meaterson for doing most things.
     clContext = WebCL.createContext({
         devices: device,
         platform: platform
     });
 
     // CL Queue setup
-    // The Queue is, as the name implies, where we will be queueing kernels to
+    // The Queue is, as the name implies, where we will be clQueueing kernels to
     // execute on our device.
     clQueue = clContext.createCommandQueue(device, 0);
 }
 
-function TiltShift (imageName, upperBoundary, lowerBoundary) {
-    // load image
+function InitTiltShiftSystem (imageFile)
+{
+    // Load original image
     var file = imageName;
     console.log('Loading image ' + file);
 
@@ -74,14 +79,135 @@ function TiltShift (imageName, upperBoundary, lowerBoundary) {
     // Convert the image to make life easy and breezy
     var image = img.convertTo32Bits();
 
+    // Create blurred image
+    // Box processing params
+    var iRadius = 14;                           // initial radius of 2D box filter mask
+    var fScale = 1/(2 * iRadius + 1);  // precalculated GV rescaling value
+
+    // OpenCL variables
+    var ckBoxRowsTex;             // OpenCL Kernel for row sum (using 2d Image/texture)
+    var ckBoxColumns;             // OpenCL for column sum and normalize
+    var cmDevBufIn;               // OpenCL device memory object (buffer or 2d Image) for input data
+    var cmDevBufTemp;             // OpenCL device memory temp buffer object
+    var cmDevBufOut;              // OpenCL device memory output buffer object
+    var szBuffBytes;              // Size of main image buffers
+    var szGlobalWorkSize=[0,0];      // global # of work items
+    var szLocalWorkSize= [0,0];       // work group # of work items
+    var szMaxWorkgroupSize = 512; // initial max # of work items
+
+    var szBuffBytes = image.height*image.pitch;
+
+    //2D Image (Texture) on device
+    cmDevBufIn = clContext.createImage(WebCL.MEM_READ_ONLY | WebCL.MEM_USE_HOST_PTR, InputFormat, image.buffer);
+
+    RowSampler = clContext.createSampler(false, WebCL.ADDRESS_CLAMP, WebCL.FILTER_NEAREST);
+
+    // Allocate the OpenCL intermediate and result buffer memory objects on the device GMEM
+    cmDevBufTemp = clContext.createBuffer(WebCL.MEM_READ_WRITE, szBuffBytes);
+    cmDevBufOut = clContext.createBuffer(WebCL.MEM_WRITE_ONLY, szBuffBytes);
+
+    //Create the program
+    clProgram = clContext.createProgram("tilt_shift.cl");
+
+    sBuildOpts = "-cl-fast-relaxed-math";
+    ciErrNum = clProgram.build(device, sBuildOpts);
+
+    ckBoxRowsTex = clProgram.createKernel("BoxRowsTex");
+    ckBoxColumns = clProgram.createKernel("BoxColumns");
+
+    // set the kernel args
+    ResetKernelArgs(image.width, image.height, iRadius, fScale);
+
+    // launch processing on the GPU
+    BoxFilterGPU (image, cmDevBufOut, iRadius, fScale);
+    clQueue.finish();
+
+    // Copy results back to host memory, block until complete
+    var uiOutput=new Uint8Array(szBuffBytes);
+    clQueue.enqueueReadBuffer(cmDevBufOut, WebCL.TRUE, 0, szBuffBytes, uiOutput);
+
+    // PNG uses 32-bit images, JPG can only work on 24-bit images
+    if(!Image.save('out_'+iRadius+'.png',uiOutput, image.width,image.height, image.pitch, image.bpp, 0xFF0000, 0x00FF00, 0xFF))
+      log("Error saving image");
+}
+
+function ResetKernelArgs(width, height, r, fScale)
+{
+    // (Image/texture version)
+    ckBoxRowsTex.setArg(0, cmDevBufIn);
+    ckBoxRowsTex.setArg(1, cmDevBufTemp);
+    ckBoxRowsTex.setArg(2, RowSampler);
+    ckBoxRowsTex.setArg(3, width, WebCL.type.UINT);
+    ckBoxRowsTex.setArg(4, height, WebCL.type.UINT);
+    ckBoxRowsTex.setArg(5, r, WebCL.type.INT);
+    ckBoxRowsTex.setArg(6, fScale, WebCL.type.FLOAT);
+
+    // Set the Argument values for the column kernel
+    ckBoxColumns.setArg(0, cmDevBufTemp);
+    ckBoxColumns.setArg(1, cmDevBufOut);
+    ckBoxColumns.setArg(2, width, WebCL.type.UINT);
+    ckBoxColumns.setArg(3, height, WebCL.type.UINT);
+    ckBoxColumns.setArg(4, r, WebCL.type.INT);
+    ckBoxColumns.setArg(5, fScale, WebCL.type.FLOAT);
+}
+
+//OpenCL computation function for GPU:
+//Copies input data to the device, runs kernel, copies output data back to host
+//*****************************************************************************
+function BoxFilterGPU(image, cmOutputBuffer, r, fScale)
+{
+    // Setup Kernel Args
+    ckBoxColumns.setArg(1, cmOutputBuffer);
+
+    // Copy input data from host to device
+    var szTexOrigin = [0, 0, 0];                // Offset of input texture origin relative to host image
+    var szTexRegion = [image.width, image.height, 1];   // Size of texture region to operate on
+    log('enqueue image: origin='+szTexOrigin+", region="+szTexRegion);
+    clQueue.enqueueWriteImage(cmDevBufIn, WebCL.TRUE, szTexOrigin, szTexRegion, 0, 0, image.buffer);
+
+    // Set global and local work sizes for row kernel
+    szLocalWorkSize[0] = 1;
+    szLocalWorkSize[1] = 1;
+    szGlobalWorkSize[0]= szLocalWorkSize[0] * clu.DivUp(image.height, szLocalWorkSize[0]);
+    szGlobalWorkSize[1] = 1;
+    log("row kernel work sizes: global="+szGlobalWorkSize+" local="+szLocalWorkSize);
+
+    //Sync host
+    clQueue.finish();
+
+    //Launch row kernel
+    clQueue.enqueueNDRangeKernel(ckBoxRowsTex, null, szGlobalWorkSize, szLocalWorkSize);
+
+    //Set global and local work sizes for column kernel
+    szLocalWorkSize[0] = 1;
+    szLocalWorkSize[1] = 1;
+    szGlobalWorkSize[0] = szLocalWorkSize[0] * clu.DivUp(image.width, szLocalWorkSize[0]);
+    szGlobalWorkSize[1] = 1;
+    log("column kernel work sizes: global="+szGlobalWorkSize+" local="+szLocalWorkSize);
+
+    //Launch column kernel
+    clQueue.enqueueNDRangeKernel(ckBoxColumns, null, szGlobalWorkSize, szLocalWorkSize);
+
+    //sync host
+    clQueue.finish();
+}
+
+function TiltShift (imageName, upperBoundary, lowerBoundary) {
+    // Get slider values
+
+    // Compute composite image
+
+    // Display image
+
+
+
+
     // The size of a buffer to hold the image once manipulated
     var szBuffBytes = image.height*image.pitch;
 
     var outputBytes = new Uint8Array(szBuffBytes);
     var outputImageBuffer;
     var inputImage;
-
-    InitWebCL();
 
     // Allocate OpenCL object for the image source data
     var InputFormat = {
@@ -131,7 +257,7 @@ function TiltShift (imageName, upperBoundary, lowerBoundary) {
     //  slice_pitch, ptr, optional event_list, optional event)
     clQueue.enqueueWriteImage(inputImage, WebCL.TRUE, [0, 0, 0], [image.width, image.height, 1], 0, 0, image.buffer);
 
-    // Finish all tasks in the queue before continuing.
+    // Finish all tasks in the clQueue before continuing.
     // (The image has to be written before it can be read)
     clQueue.finish();
 
@@ -153,5 +279,6 @@ function TiltShift (imageName, upperBoundary, lowerBoundary) {
         console.log("Error saving image");
 }
 
-
-TiltShift("old_test.png", 100, 100);
+InitWebCL();
+InitTiltShiftSystem("old_test.png");
+// TiltShift(originalImage, blurredImage, upperBoundary, lowerBoundary);
---
+++ clUtils.js
@@ -0,0 +1,239 @@
+// Copyright (c) 2011-2012, Motorola Mobility, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of the Motorola Mobility, Inc. nor the names of its
+//    contributors may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+function min(a,b) { return a<b ? a : b; }
+function max(a,b) { return a>b ? a : b; }
+exports.clamp=function clamp(a,b,c) { return min(max(a,b),c); }
+exports.min=min;
+exports.max=max;
+
+exports.sprintf=function () {
+  // Return a formatted string  
+  // 
+  // version: 1109.2015
+  // discuss at: http://phpjs.org/functions/sprintf    // +   original by: Ash Searle (http://hexmen.com/blog/)
+  // + namespaced by: Michael White (http://getsprink.com)
+  // +    tweaked by: Jack
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Paulo Freitas    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Brett Zamir (http://brett-zamir.me)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // *     example 1: sprintf("%01.2f", 123.1);
+  // *     returns 1: 123.10    // *     example 2: sprintf("[%10s]", 'monkey');
+  // *     returns 2: '[    monkey]'
+  // *     example 3: sprintf("[%'#10s]", 'monkey');
+  // *     returns 3: '[####monkey]'
+  var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;    
+  var a = arguments,
+  i = 0,
+  format = a[i++];
+
+  // pad()    
+  var pad = function (str, len, chr, leftJustify) {
+    if (!chr) {
+      chr = ' ';
+    }
+    var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);        
+    return leftJustify ? str + padding : padding + str;
+  };
+
+
+  //justify()
+  var justify = function (value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {        
+    var diff = minWidth - value.length;
+    if (diff > 0) {
+      if (leftJustify || !zeroPad) {
+        value = pad(value, minWidth, customPadChar, leftJustify);
+      } else {                
+        value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+      }
+    }
+    return value;
+  };
+
+  //formatBaseX()
+  var formatBaseX = function (value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+    // Note: casts negative numbers to positive ones
+    var number = value >>> 0;        
+    prefix = prefix && number && {
+      '2': '0b',
+      '8': '0',
+      '16': '0x'
+    }[base] || '';        
+    value = prefix + pad(number.toString(base), precision || 0, '0', false);
+    return justify(value, prefix, leftJustify, minWidth, zeroPad);
+  };
+
+  //formatString()    
+  var formatString = function (value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
+    if (precision != null) {
+      value = value.slice(0, precision);
+    }
+    return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);    
+  };
+
+  //  doFormat()
+  var doFormat = function (substring, valueIndex, flags, minWidth, _, precision, type) {
+    var number;        
+    var prefix;
+    var method;
+    var textTransform;
+    var value;
+    if (substring == '%%') {
+      return '%';
+    }
+
+    // parse flags        
+    var leftJustify = false,
+    positivePrefix = '',
+    zeroPad = false,
+    prefixBaseX = false,
+    customPadChar = ' ';        
+    var flagsl = flags.length;
+    for (var j = 0; flags && j < flagsl; j++) {
+      switch (flags.charAt(j)) {
+      case ' ':
+        positivePrefix = ' ';                
+        break;
+      case '+':
+        positivePrefix = '+';
+        break;
+      case '-':                
+        leftJustify = true;
+        break;
+      case "'":
+        customPadChar = flags.charAt(j + 1);
+        break;            
+      case '0':
+        zeroPad = true;
+        break;
+      case '#':
+        prefixBaseX = true;                
+        break;
+      }
+    }
+
+    // parameters may be null, undefined, empty-string or real valued        
+    // we want to ignore null, undefined and empty-string values
+    if (!minWidth) {
+      minWidth = 0;
+    } else if (minWidth == '*') {
+      minWidth = +a[i++];        
+    } else if (minWidth.charAt(0) == '*') {
+      minWidth = +a[minWidth.slice(1, -1)];
+    } else {
+      minWidth = +minWidth;
+    } 
+    // Note: undocumented perl feature:
+    if (minWidth < 0) {
+      minWidth = -minWidth;
+      leftJustify = true;        
+    }
+
+    if (!isFinite(minWidth)) {
+      throw new Error('sprintf: (minimum-)width must be finite');
+    } 
+    if (!precision) {
+      precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type == 'd') ? 0 : undefined;
+    } else if (precision == '*') {
+      precision = +a[i++];        
+    } else if (precision.charAt(0) == '*') {
+      precision = +a[precision.slice(1, -1)];
+    } else {
+      precision = +precision;
+    } 
+    // grab value using valueIndex if required?
+    value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+
+    switch (type) {        case 's':
+      return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
+    case 'c':
+      return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+    case 'b':            
+      return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'o':
+      return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'x':
+      return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);        
+    case 'X':
+      return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad).toUpperCase();
+    case 'u':
+      return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+    case 'i':        
+    case 'd':
+      number = (+value) | 0;
+      prefix = number < 0 ? '-' : positivePrefix;
+      value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+      return justify(value, prefix, leftJustify, minWidth, zeroPad);        
+    case 'e':
+    case 'E':
+    case 'f':
+    case 'F':
+    case 'g':        
+    case 'G':
+      number = +value;
+      prefix = number < 0 ? '-' : positivePrefix;
+      method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+      textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];            
+      value = prefix + Math.abs(number)[method](precision);
+      return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+    default:
+      return substring;
+    }    
+  };
+
+  return format.replace(regex, doFormat);
+}
+
+//Helper function to init data arrays 
+//*********************************************************************
+exports.fillArray=function (array, max)
+{
+  if(max==undefined) max=100;
+  for (var i = 0, l=array.length; i < l; ++i) 
+    array[i] = Math.floor(Math.random()*max);
+}
+
+//Helper function to print data arrays 
+//*********************************************************************
+exports.printArray=function (array)
+{
+  for (var i = 0, l=array.length; i < l; ++i) 
+    console.log(sprintf("%d: %d", i, array[i]));
+}
+
+//Round Up Division function
+exports.roundUp=function (group_size, global_size) 
+{
+    return (global_size % group_size == 0) ? global_size : (global_size + group_size - r);
+}
+
+//Helper to get next up value for integer division
+exports.DivUp= function (dividend, divisor)
+{
+  return (dividend % divisor == 0) ? Math.round(dividend / divisor) : Math.round(dividend / divisor) + 1;
+}
+
